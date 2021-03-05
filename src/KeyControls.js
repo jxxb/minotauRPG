dec8e425f8f7ba57e6b8d7dd0a2c79af7c1f0e05
DortchCasey <caseyd199@gmail.com>
2021-03-05 11:19:53 -0700

    colision detection and code restructure

--- src/KeyControls.js
+++ src/KeyControls.js
@@ -1,55 +1,54 @@
-export default class KeyControls{
-    constructor(){
-        this.keys = {}
-        //bind event handlers
-        document.addEventListener('keydown', e=>{
-            if([32,37,38,39,40].indexOf(e.which) >= 0){
-                e.preventDefault();
-            }
-            this.keys[e.which] = true;
-        }, false);
-        document.addEventListener('keyup', e=>{
-            this.keys[e.which] = false;
-        }, false);
+class KeyControls {
+    constructor() {
+      this.keys = {};
+      // Bind event handlers
+      document.addEventListener(
+        "keydown",
+        (e) => {
+          if (
+            ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.key) >=
+            0
+          ) {
+            e.preventDefault();
+          }
+          this.keys[e.key] = true;
+        },
+        false
+      );
+      document.addEventListener(
+        "keyup",
+        (e) => {
+          this.keys[e.key] = false;
+        },
+        false
+      );
     }
-    get action(){
-        return this.keys[32];
+    // Handle key actions
+    get action() {
+      return this.keys[" "];
     }
-    getRotate(ctx, px, py, angle){
-        const DegToRad = deg =>(Math.PI/180)*deg;
-        
-        if(this.keys[37]||this.keys[65]){
-           
-            ctx.translate(px , py ); 
-            // Perform the rotation  
-            ctx.rotate(DegToRad(angle));  
-            // Translate back to the top left of our image  
-            ctx.translate(-px, -py);
-            
-            return -1;
-        }
-        if(this.keys[39]||this.keys[68]){
-            
-            ctx.translate(px, py);  
-            // Perform the rotation  
-            ctx.rotate(DegToRad(angle));  
-            // Translate back to the top left of our image  
-            ctx.translate(-px, -py);
-           
-            return 1;
-        }
-        return 0;
+    get x() {
+      // left arrow or A key
+      if (this.keys["ArrowLeft"] || this.keys["a"]) {
+        return -1;
+      }
+      // right arrow or D key
+      if (this.keys["ArrowRight"] || this.keys["d"]) {
+        return 1;
+      }
+      return 0;
     }
-    getY(){
-        if(this.keys[38]||this.keys[87]){
-            return -1;
-        }
-        if(this.keys[40]||this.keys[83]){
-            return 1;
-        }
-        return 0;
+    get y() {
+      // up arrow or W key
+      if (this.keys["ArrowUp"] || this.keys["w"]) {
+        return -1;
+      }
+      // down arrow or S key
+      if (this.keys["ArrowDown"] || this.keys["s"]) {
+        return 1;
+      }
+      return 0;
     }
-    
-    
-
-}
+  }
+  export default KeyControls;
+  
