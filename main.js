af1d8fe1e2b006ecad4ac0242cd74d80c87261a0
jacall2016 <56280014+jacall2016@users.noreply.github.com>
2021-03-06 10:13:06 -0700

    added rotated imgs

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const textures ={
    mino: new Texture('images/enemy/Enemy.png'),
    sword: new Texture('images/weapons/sword_base-01.png')
 };
+
 const character = new Sprite(textures.character);
 character.pos.x = 120;
 character.pos.y = h/2-16;
@@ -23,10 +24,20 @@ character.update = function(dt,t){
    this.pos.x += controls.x * dt * 200;
    this.pos.y += controls.y * dt * 200;
  
-   if(this.pos.x < 24){
-     this.pos.x = 24;
+   if (controls.x == 1) {
+      textures.character.img.src = 'images/character/Character_right.png';
+   } else if (controls.x == -1) {
+      textures.character.img.src = 'images/character/Character_left.png';
+   } else if (controls.y == -1) {
+      textures.character.img.src = 'images/character/Character_base.png';
+   } else if (controls.y == 1){
+      textures.character.img.src = 'images/character/Character_down.png';
    }
-   if(this.pos.x >w){this.pos.x = w;}
+   
+   if(this.pos.x < 0){this.pos.x = 0;}
+   if(this.pos.x > w - this.size.sx){this.pos.x = w - this.size.sx;}
+   if(this.pos.y < 0){this.pos.y = 0;}
+   if(this.pos.y > h - this.size.sy){this.pos.y = h - this.size.sy;}
 }
 const minows = new Container();
 function spawnMino(x, y, speed){
@@ -40,21 +51,39 @@ function spawnMino(x, y, speed){
    };
    minows.add(mino);
 }
+
 const sword = new Sprite(textures.sword);
-function drawSword(){
 sword.pos.x = character.pos.x -80;
 sword.pos.y = character.pos.y -110;
-sword.size.sx = 200;
-sword.size.sy = 200;
-sword.update = function(dt, t){
-   this.pos.x += controls.x * dt * 200;
-   this.pos.y += controls.y * dt * 200;
+
+function drawSword(){
+   sword.size.sx = 200;
+   sword.size.sy = 200;
+   sword.update = function(dt, t){
+      this.pos.x += controls.x * dt * 200;
+      this.pos.y += controls.y * dt * 200;
+
+      if (controls.x == 1) {
+         textures.sword.img.src = 'images/weapons/sword_right.png'
+         this.pos.x = character.pos.x;
+         this.pos.y = character.pos.y - 80;
+      } else if (controls.x == -1) {
+         textures.sword.img.src = 'images/weapons/sword_left.png'
+         this.pos.x = character.pos.x - 110;
+         this.pos.y = character.pos.y - 20;
+      } else if (controls.y == 1) {
+         textures.sword.img.src = 'images/weapons/sword_down.png'
+         sword.pos.x = character.pos.x - 10;
+         sword.pos.y = character.pos.y;
+      } else if (controls.y == -1) {
+         textures.sword.img.src = 'images/weapons/sword_base-01.png'
+         sword.pos.x = character.pos.x - 80;
+         sword.pos.y = character.pos.y - 110;
+      }
  
-   if(this.pos.x < 24){
-     this.pos.x = 24;
+      if(this.pos.x + this.sx/2 < 0){this.pos.x = this.sx/2;}
+      if(this.pos.x + this.sx/2 > w){this.pos.x = w - this.sx/2;}
    }
-   if(this.pos.x >w){this.pos.x = w;}
-}
 }
 
 scene.add(sword);
