3ed483f7c3529ac7176462f7b35a82e34822e2c5
jacall2016 <56280014+jacall2016@users.noreply.github.com>
2021-03-10 16:37:13 -0700

    worked on api

--- src/ExternalServices.js
+++ src/ExternalServices.js
@@ -13,7 +13,133 @@ export default class ExternalServices {
     constructor() {}
 
     getMaze() {
+        
+        //if cell.verical = true call spawnVWall(cell);
+        const APIResponse = [
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: true },
+            { horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: true },
+            { horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: true, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: false },
+            { horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: false },
+            { horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: true },
+            { horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: true },
+            { horizontal: false },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: true },
+            { horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: true },
+            { horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: true, horizontal: false },
+            { vertical: true, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: false, horizontal: true },
+            { vertical: true, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: false },
+            { vertical: false, horizontal: true },
+            { horizontal: false },
+            { vertical: true },
+            { vertical: true },
+            { vertical: false },
+            { vertical: false },
+            { vertical: false },
+            { vertical: true },
+            { vertical: true },
+            { vertical: true },
+            { vertical: false }
+        ]
 
+
+        for(let i = 0; i < APIResponse.length; i++) {
+            for(let j = 0; j < APIResponse.length; j++) {
+                if (i.vertical) {
+
+                }
+                if (i.horizontal) {
+
+                }
+            }
+        }
+            if (cell.vertical) {
+                const vWall = new Sprite(textures.vWall);
+                textures.vWall.img.src = 'images/wall/wall_vertical.png';
+                
+                vWall.size.sx = 20;
+                vWall.size.sy = 100;
+                vWall.pos.x = x;
+                vWall.pos.y = y;
+                vWalls.add(vWall);
+            }
+            return vWall;
+        });
     }
 
     saveMaze() {
@@ -23,13 +149,17 @@ export default class ExternalServices {
     async loginRequest(creds) {
         const options = {
             method: 'POST',
+            body:JSON.stringify(creds),
             header: {
                 'Content-Type':'application/json'
             },
-            body:JSON.stringify(creds),
         };
-        const response = await fetch(base_url + 'login', options).then(convertToJson);
+        const response = await fetch(base_url + 'signin', options).then(convertToJson);
         console.log(response);
         return response;//.accessToken;
+    }
+
+    async signUpRequest(creds) {
+        const options
     }
 }
