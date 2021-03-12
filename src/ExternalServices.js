30157391d2fc82683f14797bb1abe54bccbe0972
Jared Barney <jared.barney97@gmail.com>
2021-03-12 12:55:59 -0700

    maze

--- src/ExternalServices.js
+++ src/ExternalServices.js
@@ -197,7 +197,7 @@ export default class ExternalServices {
         const options = {
             method: 'POST',
             body:JSON.stringify(creds),
-            header: {
+            headers: {
                 'Content-Type':'application/json'
             },
         };
